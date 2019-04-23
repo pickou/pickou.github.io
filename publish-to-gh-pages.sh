3f649019919f8a5479528d42f39a791c491c6650
jcyan <yanjunchao@ict.ac.cn>
2019-04-23 16:13:01 +0800

    travis auto deploy

---
+++ publish-to-gh-pages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ev
+git clone https://${GH_REF} .deploy_git
+cd .deploy_git
+git checkout master
+cd ../
+mv .deploy_git/.git/ ./public/
+cd ./public
+git config user.name  "pickou"
+git config user.email "472179216@qq.com" 
+# add commit timestamp
+git add .
+git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
+git push --force --quiet "https://${Travis_gh_token}@${GH_REF}" master:master
+#git push --force --quiet "https://hadronw:${Travis_co_token}@${CO_REF}" master:master
