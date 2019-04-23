71c46c9207e5267047fb1f2b101eb6bdd0415c8c
jcyan <yanjunchao@ict.ac.cn>
2019-04-23 16:30:17 +0800

    github token name

--- publish-to-gh-pages.sh
+++ publish-to-gh-pages.sh
@@ -11,5 +11,5 @@ git config user.email "472179216@qq.com"
 # add commit timestamp
 git add .
 git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
-git push --force --quiet "https://${Travis_gh_token}@${GH_REF}" master:master
+git push --force --quiet "https://${travis_gh_token}@${GH_REF}" master:master
 #git push --force --quiet "https://hadronw:${Travis_co_token}@${CO_REF}" master:master
